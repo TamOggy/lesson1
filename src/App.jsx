20afee2939d8536e88cf9b456b36d59c4244c3d2
TamOggy <antam823@gmail.com>
2025-04-02 14:38:31 +0700

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,72 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [taskInput, setTaskInput] = useState('');
+  const [tasks, setTasks] = useState(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  });
 
+  const [taskInput, setTaskInput] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = () => {
     if (taskInput.trim() !== "") {
-      setTasks([...tasks, taskInput]);
-      setTaskInput(""); 
+      setTasks((previousTasks) => [
+        ...previousTasks,
+        { task: taskInput, completed: false },
+      ]);
+      setTaskInput("");
     }
   };
 
   const deleteTask = (taskToDelete) => {
-    setTasks(tasks.filter(task => task !== taskToDelete));
+    const updatedTasks = tasks.filter((task) => task.task !== taskToDelete);
+    setTasks(updatedTasks);
+  };
+
+  const toggleComplete = (taskToToggle) => {
+    const updatedTasks = tasks.map((task) =>
+      task.task === taskToToggle
+        ? { ...task, completed: !task.completed }
+        : task
+    );
+    setTasks(updatedTasks);
   };
 
   return (
     <div className="container">
-      <h1 className="content">To-Do List</h1>
-      <input 
-        className="inputask" 
-        type="text" 
-        placeholder="Add new task ?" 
-        value={taskInput} 
-        onChange={(e) => setTaskInput(e.target.value)} 
-      />
-      <button className="addTask" onClick={addTask}>Add Task</button>
+      <h1 className="content">TO-DO LIST</h1>
+      <div className="container-body">
+        <input
+          className="inputask"
+          type="text"
+          placeholder="Add new task ?"
+          value={taskInput}
+          onChange={(e) => setTaskInput(e.target.value)}
+        />
+        <button className="addTask" onClick={addTask}>
+          Add
+        </button>
+      </div>
       <ul className="taskList">
         {tasks.map((task, index) => (
           <li key={index}>
-            {task}
-            <button className="delete" onClick={() => deleteTask(task)}>Delete</button>
+            <input
+              type="checkbox"
+              className="checkbox-list"
+              checked={task.completed}
+              onChange={() => toggleComplete(task.task)}
+            />
+            <p className={`taskList-info ${task.completed ? "completed" : ""}`}>
+              {task.task}
+            </p>
+            <button className="delete" onClick={() => deleteTask(task.task)}>
+              <span className="material-symbols-outlined">close</span>
+            </button>
           </li>
         ))}
       </ul>
